1b3dca711a95e39fa6297ee3c37a3a4b3350635f
Mayank <mayankmishra9137@gmail.com>
2025-09-20 14:00:24 +0000

    variables and datatype

---
+++ 01_basic.js/01_variables.js
@@ -0,0 +1,17 @@
+const accountID = 12345;
+let accountEmail = "mayankmishra@gmail.com"
+var accountPassword = "12345";
+let accountState;
+// accountCity = "jaipur";
+
+// accountID = 2; // not allowed
+
+accountEmail = "hchc@gmail.com"
+accountPassword = "67890";
+// accountCity = "dlh";
+/*
+prefer not to use var
+*/
+
+
+console.table([accountID, accountEmail, accountPassword,accountState]);
---
+++ 01_basic.js/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3);
+// code readability is very important
+console.log("Mayank");
+
+let name = "mayank";
+let age = 21;
+let isLoggedIn = false;
+let state = null;
+let city;
+
+// number => 2 to power 53;
+//bigint;
+//string => "mayank"
+//boolean => true/false
+//null => standalone value
+//underfined => value not assigned
+//symbol => unique
+
+// object 
+
+console.log(typeof null);// object  // to find datatype of variable
