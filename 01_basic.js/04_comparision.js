b8b3c09e46fa7f71195aa6e963d6e48c1928e187
Mayank <mayankmishra9137@gmail.com>
2025-09-21 18:14:21 +0000

    commit

---
+++ 01_basic.js/04_comparision.js
@@ -0,0 +1,31 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);  avoid null and undefined 
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+
+// === 
+// strict check= not only check value but also datatype
+
+console.log("2" === 2);
+
+
+
+
+
+
+
